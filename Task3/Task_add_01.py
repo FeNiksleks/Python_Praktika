3bc76af702eb3e9918e70c7e6adfaca4b4de4a94
FeNiksleks <feniksleks@yandex.ru>
2023-06-22 21:42:33 +0300

    выполнили задание семинара

---
+++ Task3/Task_add_01.py
@@ -0,0 +1,19 @@
+# list,set,dct - изменяемые
+# import time
+
+# a = [i for i in range(15000000)]
+# start = time.perf_counter()
+# if 130000 in a:
+#     print("нашел в списке")
+# end = time.perf_counter()
+# print(end-start)
+
+# dct = {i:1 for i in range(15000000)}
+# start = time.perf_counter()
+# if 130000 in dct:
+#     print("нашел в словаре")
+# end = time.perf_counter()
+# print(end-start)
+
+
+
